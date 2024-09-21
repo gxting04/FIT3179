1089b282963bf4fb9c4430949747f82b1e5873bb
gxting04 <99959404+gxting04@users.noreply.github.com>
2024-09-21 16:48:15 +0800

    Files for Homework Assessment 3

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg1 = 'map.json';
+
+vegaEmbed("#map", vg1).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
