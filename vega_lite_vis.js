ab34bf837d672edca3be962ac2213ca183f24ff6
gxting04 <99959404+gxting04@users.noreply.github.com>
2024-10-06 14:37:08 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,12 @@
 var vg1 = 'map.json';
+var vg2 = 'barchart.json';
 
 vegaEmbed("#map", vg1).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#bar", vg2).then(function(result){
     // Access the Vega view instance 
     // https://vega.github.io/vega/docs/api/view/ as result.view
 }).catch(console.error)
